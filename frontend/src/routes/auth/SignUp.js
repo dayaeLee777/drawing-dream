7f3b6af753a063e1976d78ddfd33a1b37ebedaa9
제진명 <1995jjm@naver.com>
2022-01-28 00:35:59 +0900

    [S06P12A607-118] refector: 유효성 검사 코드 분리

--- frontend/src/routes/auth/SignUp.js
+++ frontend/src/routes/auth/SignUp.js
@@ -7,9 +7,9 @@ import logo from "assets/img/logo.png";
 import NewWindow from "react-new-window";
 import PostCode from "components/signup/postcode/FindPostCode";
 import SchoolCode from "components/signup/school/SchoolCode";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { idCheck, signUp } from "api/user";
+import validationCheck from "components/signup/validationCheck";
+import { signUp } from "api/user";
 import { ReactNewWindowStyles } from "react-new-window-styles";
 
 const Container = styled.div`
@@ -175,163 +175,8 @@ const SignUp = () => {
 
   // 유효성 검사
   const onChange = (e) => {
-    let { value, name } = e.target;
-    // console.log(value);
-
-    if (name === "userId") {
-      const regType1 = /^[a-z0-9+]*$/;
-      if (value.length < 6) {
-        setValids({
-          ...valids,
-          validId: false,
-        });
-        setErrors({
-          ...errors,
-          idErrMsg: "6자리 이상 입력해주세요.",
-        });
-      } else if (!regType1.test(value)) {
-        setValids({
-          ...valids,
-          validId: false,
-        });
-        setErrors({
-          ...errors,
-          idErrMsg: "알파벳 소문자와 숫자만 사용가능합니다.",
-        });
-      }
-      // id 중복 검사
-      else if (value.length >= 6) {
-        idCheck(value).then((res) => {
-          if (res.data.statusCode === 200) {
-            setValids({
-              ...valids,
-              validId: false,
-            });
-            setErrors({
-              ...errors,
-              idErrMsg: "이미 사용중인 아이디입니다.",
-            });
-          } else if (res.data.statusCode === 409) {
-            setValids({
-              ...valids,
-              validId: true,
-            });
-            setErrors({
-              ...errors,
-              idErrMsg: "사용가능한 아이디입니다.",
-            });
-          }
-        });
-      }
-    } else if (name === "userName") {
-      const regExp = /[ㄱ-ㅎㅏ-ㅣ가-힣]/g;
-      const regExp2 = /[ㄱ-ㅎㅏ-ㅣ]/g;
-
-      if (!regExp.test(value) || regExp2.test(value)) {
-        setValids({
-          ...valids,
-          validName: false,
-        });
-        setErrors({
-          ...errors,
-          nameErrMsg: "이름을 정확히 입력해 주세요.",
-        });
-      } else {
-        setValids({
-          ...valids,
-          validName: true,
-        });
-        setErrors({
-          ...errors,
-          nameErrMsg: "",
-        });
-      }
-    } else if (name === "password") {
-      if (value.length < 8) {
-        setValids({
-          ...valids,
-          validPassword: false,
-        });
-        setErrors({
-          ...errors,
-          passwordErrMsg: "8자리 이상 입력하세요.",
-        });
-      } else if (value.length > 20) {
-        setValids({
-          ...valids,
-          validPassword: false,
-        });
-        setErrors({
-          ...errors,
-          passwordErrMsg: "20자리 이하로 입력하세요.",
-        });
-      } else {
-        setValids({
-          ...valids,
-          validPassword: true,
-        });
-        setErrors({
-          ...errors,
-          passwordErrMsg: "",
-        });
-      }
-      const regExp = /\s/gi;
-      value = value.replace(regExp, "");
-    } else if (name === "passwordConfirm") {
-      if (password === value) {
-        setValids({
-          ...valids,
-          equelPassword: true,
-        });
-        setErrors({
-          ...errors,
-          equelPasswordErrMsg: "",
-        });
-      } else {
-        setValids({
-          ...valids,
-          equelPassword: false,
-        });
-        setErrors({
-          ...errors,
-          equelPasswordErrMsg: "비밀번호가 일치하지 않습니다.",
-        });
-      }
-    } else if (name === "phoneNumber" || name === "parentPhoneNumber") {
-      const regType1 = /[^0-9]/;
-      value = value.replace(regType1, "");
-    } else if (name === "email") {
-      const regExp =
-        /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
-      if (regExp.test(value)) {
-        setValids({
-          ...valids,
-          validEmail: true,
-        });
-        setErrors({
-          ...errors,
-          emailErrMsg: "",
-        });
-      } else {
-        setValids({
-          ...valids,
-          validEmail: false,
-        });
-        setErrors({
-          ...errors,
-          emailErrMsg: "이메일형식이 올바르지 않습니다.",
-        });
-      }
-    } else if (name === "studentNo") {
-      const regType1 = /[^0-9]/;
-      value = value.replace(regType1, "");
-    }
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
+    validationCheck(e, inputs, setInputs, valids, setValids, errors, setErrors);
   };
-  // 유효성 검사 END
 
   // ADDRESS, SCHOOL INFO STATE
   const [fullAddress, setFullAddress] = useState("");
