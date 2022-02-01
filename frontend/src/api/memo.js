00d4ba8426c2d10f74924913eb38cc2f03faf446
runkey <csi9509@naver.com>
2022-02-01 16:18:54 +0900

    "feat: 메모 api 연결"

--- frontend/src/api/memo.js
+++ frontend/src/api/memo.js
@@ -1,9 +1,21 @@
 import {api} from 'api/api'
 
-export const getMemoList = async (success, fail) => {
-  return await api.get("/api/memo/list").then(success).catch(fail);
+export const registerMemo = async (content, success, fail) => {
+  return await api.post("/api/memo", content).then(success).catch(fail);
 };
 
 export const getMemo = async (memoId, success, fail) => {
   return await api.get(`/api/memo/${memoId}`).then(success).catch(fail);
+};
+
+export const getMemoList = async (success, fail) => {
+  return await api.get("/api/memo/list").then(success).catch(fail);
+};
+
+export const modifyMemo = async (content, success, fail) => {
+  return await api.put("api/memo", content).then(success).catch(fail);
+};
+
+export const deleteMemo = async (memoId, success, fail) => {
+  return await api.put(`api/memo/delete/${memoId}`).then(success).catch(fail);
 };
