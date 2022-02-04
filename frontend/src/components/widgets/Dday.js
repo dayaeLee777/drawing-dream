f45e17791c2d31733d3ac906f26a52084d242921
jubi in <thetruthrunswild@naver.com>
2022-02-04 20:30:26 +0900

    [S06P12A607-218] feat: 학사일정 위젯 구성
    testData로 calendar와 D day 기능 구현

--- frontend/src/components/widgets/Dday.js
+++ frontend/src/components/widgets/Dday.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import commonCode from "config/commonCode";
 
 const Container = styled(motion.div)`
   /* height: 15rem; */
@@ -23,6 +25,23 @@ const CloseButton = styled.div`
   display: flex;
   align-items: center;
 `;
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 60%;
+`;
+
+const Content = styled.div`
+  font-weight: 600;
+  font-size: 4rem;
+  margin-bottom: 2rem;
+`;
+
+const Name = styled.div`
+  font-size: 1.5rem;
+`;
 
 const Dday = ({
   isShow,
@@ -32,6 +51,8 @@ const Dday = ({
   widgetId,
   setWidgetId,
 }) => {
+  const [recentDday, setRecentDday] = useState("");
+  const [DdayName, setDdayName] = useState("");
   const close = () => {
     const newIsShow = isShow.filter((wid) => {
       return wid !== widgetId;
@@ -39,6 +60,36 @@ const Dday = ({
     setIsShow(newIsShow);
     setIsntShow([...isntShow, widgetId]);
   };
+
+  const testData = [
+    {
+      id: "3bc288c2-b9c3-92c2-b9c3-8d47c2acc2ae",
+      title: commonCode.J.J01,
+      start: "2022-02-02",
+      end: "2022-02-02",
+    },
+    {
+      title: commonCode.J.J07,
+      id: "c3b5c2a2-c3a4-24c2-84c2-86497fc2b539",
+      start: "2022-02-23",
+      end: "2022-02-25",
+    },
+  ];
+
+  useEffect(() => {
+    const today = new Date();
+    testData.forEach((data) => {
+      const date = new Date(data.start);
+      const dday = date.getTime() - today.getTime();
+      if (dday >= 0) {
+        const result = Math.ceil(dday / (1000 * 60 * 60 * 24));
+        setRecentDday(result);
+        setDdayName(data.title);
+        return;
+      }
+    });
+  }, []);
+
   return (
     <Container
       layout
@@ -54,6 +105,10 @@ const Dday = ({
         <Title>D-DAY</Title>
         {setIsShow && <CloseButton onClick={close}>❌</CloseButton>}
       </Wrapper>
+      <ContentContainer>
+        <Content>D-{recentDday}</Content>
+        <Name>{DdayName}</Name>
+      </ContentContainer>
     </Container>
   );
 };
--- frontend/src/components/widgets/modals/DdayModal.js
+++ frontend/src/components/widgets/modals/DdayModal.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import styled from "styled-components";
+import Calendar from "@toast-ui/react-calendar";
+import "tui-calendar/dist/tui-calendar.css";
+import commonCode from "config/commonCode";
 
 const Wrapper = styled(motion.div)`
-  width: 600px;
-  height: 600px;
+  width: 800px;
+  height: 800px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -32,16 +35,50 @@ const Title = styled.div`
   margin-bottom: 20px;
 `;
 
+const Content = styled.div`
+  width: 80%;
+  height: 80%;
+`;
 const DdayModal = ({ layoutId }) => {
   const onClick = (event) => {
     event.stopPropagation();
   };
+
+  const date1 = new Date("2022-02-02");
+  console.log(date1.toString);
+  const testData = [
+    {
+      calendarId: "3bc288c2-b9c3-92c2-b9c3-8d47c2acc2ae",
+      category: "time",
+      isVisible: true,
+      isPending: false,
+      title: commonCode.J.J01,
+      id: "3bc288c2-b9c3-92c2-b9c3-8d47c2acc2ae",
+      body: "테스트입니다",
+      start: "2022-02-02",
+      end: "2022-02-02",
+    },
+    {
+      calendarId: "c3b5c2a2-c3a4-24c2-84c2-86497fc2b539",
+      category: "allday",
+      isVisible: true,
+      isPending: false,
+      title: commonCode.J.J07,
+      id: "c3b5c2a2-c3a4-24c2-84c2-86497fc2b539",
+      body: "테스트입니다",
+      start: "2022-02-23",
+      end: "2022-02-25",
+    },
+  ];
 
   return (
     <Wrapper onClick={onClick} layoutId={layoutId}>
       <Header>
         <Title>D-DAY</Title>
       </Header>
+      <Content>
+        <Calendar height="400px" view="month" schedules={testData} />
+      </Content>
     </Wrapper>
   );
 };
