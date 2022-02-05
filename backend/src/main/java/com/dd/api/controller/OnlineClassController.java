3ce03d4b0ef50ea4564d98e15fcc226fbc2a83ad
kibum414 <kibum414@naver.com>
2022-02-06 04:18:14 +0900

    [S06P12A607-162] feat: Create, Delete Online Class API

---
+++ backend/src/main/java/com/dd/api/controller/OnlineClassController.java
@@ -0,0 +1,68 @@
+package com.dd.api.controller;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dd.api.dto.request.OnlineClassRegisterRequestDTO;
+import com.dd.api.dto.response.OnlineClassResponseDTO;
+import com.dd.api.service.OnlineClassService;
+import com.dd.common.model.BaseResponseDto;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Api(value = "온라인 수업 API", tags = { "Online Class" })
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/online")
+public class OnlineClassController {
+
+	private final OnlineClassService onlineClassService;
+
+	@ApiOperation(value = "온라인 수업 생성")
+	@PostMapping
+	public ResponseEntity<? extends BaseResponseDto> createClass(
+			@ApiParam(value = "수업 코드") @RequestBody OnlineClassRegisterRequestDTO onlineClassRegisterRequestDTO,
+			@ApiIgnore @RequestHeader("Authorization") String accessToken) {
+
+		System.out.println("OnlineClassController create : " + onlineClassRegisterRequestDTO);
+
+		OnlineClassResponseDTO onlineClassResponseDTO = onlineClassService.createClass(onlineClassRegisterRequestDTO,
+				accessToken);
+
+		if (onlineClassResponseDTO == null)
+			return ResponseEntity.status(409).body(BaseResponseDto.of(409, "Fail"));
+
+		return ResponseEntity.ok(OnlineClassResponseDTO.of(200, "Success", onlineClassResponseDTO));
+
+	}
+
+	@ApiOperation(value = "온라인 수업 삭제")
+	@DeleteMapping("/{classId}")
+	public ResponseEntity<? extends BaseResponseDto> deleteClass(
+			@ApiParam(value = "수업 코드") @PathVariable UUID classId,
+			@ApiIgnore @RequestHeader("Authorization") String accessToken) {
+
+		System.out.println("OnlineClassController deleteClass : " + classId);
+
+		int result = onlineClassService.deleteClass(classId, accessToken);
+
+		if (result == 0)
+			return ResponseEntity.status(409).body(BaseResponseDto.of(409, "Fail"));
+
+		return ResponseEntity.ok(BaseResponseDto.of(200, "Success"));
+
+	}
+
+}
--- backend/src/main/java/com/dd/db/entity/onlineclass/OnlineClass.java
+++ backend/src/main/java/com/dd/db/entity/onlineclass/OnlineClass.java
@@ -1,24 +1,18 @@
 package com.dd.db.entity.onlineclass;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.dd.db.entity.BaseEntity;
-import com.dd.db.entity.user.Auth;
-import com.dd.db.entity.user.User;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @Builder
@@ -27,13 +21,12 @@ import lombok.Setter;
 @Entity
 public class OnlineClass extends BaseEntity {
 
-	@Temporal(TemporalType.TIME)
-	private Date startTime;
+	private LocalDateTime startTime;
 
-	@Temporal(TemporalType.TIME)
-	private Date endTime;
-	
+	private LocalDateTime endTime;
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "course_id")
 	private Course course;
+
 }
