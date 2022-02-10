72415e7e28a699854a4f64a7ec5540f60e6fc887
runkey <csi9509@naver.com>
2022-02-11 03:25:12 +0900

    feat: 성적 조회 기능 추가

--- frontend/src/components/widgets/score/Chart.js
+++ frontend/src/components/widgets/score/Chart.js
@@ -6,46 +6,99 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const Container = styled.div`
-  width: 80%;
+  width: 95%;
   height: 90%;
+
+  & > div > div > svg{
+    height: 500px;
+  }
 `;
 
 const Chart = () => {
-  const [data, setData] = useState([]);
   const [avg, setAvg] = useState([]);
-  const [record, setRecord] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [record, setRecord] = useState(new Array(24).fill(0));
+  const [subCnt, setSubCnt] = useState(new Array(24).fill(0)); // 1~6학년, 1, 2학기 중간, 기말
+  const label = []
+
   useEffect(() => {
     getScore().then((res) => {
       const score = res.data.scoreGetResponseDtoList;
-      setData(score);
       let idx = 0;
+      let score_temp = record.concat();
+      let subject_temp = subCnt.concat();
       for(idx; idx < score.length; idx++) {
-        console.log(score[idx].gradeCode)
-        const _grade = (score[idx].gradeCode.substring(2,3) - 1)
-        const _class = (score[idx].testCode.substring(4,5) - 1)
-        console.log(record[_grade * 4 + _class])
-        const temp = record.concat();
-        console.log(temp)
-        record[_grade * 4 + _class].push(record[_grade * 4 + _class] + score[idx].score)
+        const _grade = Number((score[idx].gradeCode.substring(2,3) - 1))
+        const _semester = Number((score[idx].semesterCode.substring(2,3) - 1))
+        const _class = Number((score[idx].testCode.substring(4,5) - 1))
+        score_temp[_grade * 4 + _semester * 2 + _class]=(score_temp[_grade * 4 + _semester * 2 + _class] + score[idx].score)
+        subject_temp[_grade * 4 + _semester * 2 + _class]=(subject_temp[_grade * 4 + _semester * 2 + _class] + 1)
+        if (idx === score.length - 1){
+          let avg_temp = avg.concat();
+          for (let avg_idx = 0 ; avg_idx < record.length; avg_idx++){
+            if(subject_temp[avg_idx] !== 0) {
+              avg_temp.push(score_temp[avg_idx] / subject_temp[avg_idx])
+            }
+          }
+          setAvg(avg_temp)
+        }
       }
+      setRecord(score_temp)
+      setSubCnt(subject_temp)
     }
     );
   }, []);
 
-  const onClick = () => {
-    console.log(data)
-    console.log(record)
-  };
+  const labels = () => {
+
+    const E = commonCode.E
+    const O = commonCode.O
+    const J = commonCode.J.J10
+    
+
+    for(let idx = 0 ; idx < avg.length ; idx++){
+      if (parseInt (idx / 4) === 0){
+      label.push([[E.E01]])
+      }
+      else if (parseInt (idx / 4) === 1){
+        label.push([E.E02])
+      }
+      else if (parseInt (idx / 4) === 2){
+        label.push([E.E03])
+      }
+      else if (parseInt (idx / 4) === 3){
+        label.push([E.E04])
+      }
+      else if (parseInt (idx / 4) === 4){
+        label.push([E.E05])
+      }
+      else if (parseInt (idx / 4) === 5){
+        label.push([E.E06])
+      }
+
+      if (idx % 4 === 0 || idx % 4 === 1){
+        label[idx][0] = label[idx][0] + O.O01
+      }
+      else if (idx % 4 === 2 || idx % 4 === 3){
+        label[idx][0] = label[idx][0] + O.O02
+      }
+
+      if (idx % 2 === 0){
+        label[idx][0] = label[idx][0] + J.J1001
+      }
+      else if (idx % 2 === 1){
+        label[idx][0] = label[idx][0] + J.J1002
+      }
+  }
+}
 
   return (
-    <Container>
-      <div onClick={onClick}>가나다</div>
+    <Container onChange={labels()}>
       <ApexCharts
         type="bar"
         series={[
           {
             name: "평균",
-            data: [100, 89, 93, 76, 80, 78, 40, 56],
+            data: avg,
           },
         ]}
         options={{
@@ -70,24 +123,8 @@ const Chart = () => {
             show: false,
           },
           xaxis: {
-            categories: [
-              [commonCode.E.E01, commonCode.J.J10.J1001],
-              [commonCode.E.E01, commonCode.J.J10.J1002],
-              [commonCode.E.E01, commonCode.J.J10.J1003],
-              [commonCode.E.E01, commonCode.J.J10.J1004],
-              [commonCode.E.E02, commonCode.J.J10.J1001],
-              [commonCode.E.E02, commonCode.J.J10.J1002],
-              [commonCode.E.E02, commonCode.J.J10.J1003],
-              [commonCode.E.E02, commonCode.J.J10.J1004],
-              // ["1학년 1학기", "중간고사"],
-              // ["1학년 1학기", "기말고사"],
-              // ["1학년 2학기", "중간고사"],
-              // ["1학년 2학기", "기말고사"],
-              // ["2학년 1학기", "중간고사"],
-              // ["2학년 1학기", "기말고사"],
-              // ["2학년 2학기", "중간고사"],
-              // ["2학년 2학기", "기말고사"],
-            ],
+            categories: label,
+            offsetX: 10,
           },
           labels: {
             style: {
@@ -99,11 +136,6 @@ const Chart = () => {
             min: 0,
             max: 100,
           },
-          // tooltip: {
-          //   y: {
-          //     formatter: (value) => `$${value.toFixed(2)}`,
-          //   },
-          // },
         }}
       />
     </Container>
