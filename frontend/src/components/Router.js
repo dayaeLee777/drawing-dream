39356f4d1e0d6f9ca51b91e85d3210b7d72fe752
제진명 <1995jjm@naver.com>
2022-02-13 16:35:57 +0900

    feat: 비밀번호 변경 기능 구현

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -25,7 +25,6 @@ import NoticeList from "./notice/NoticeList";
 import NoticeDetail from "./notice/NoticeDetail";
 import Widget from "routes/content/Widget";
 import HomeOther from "routes/HomeOther";
-import ModifyPassword from "routes/auth/ModifyPassword";
 
 const AppRouter = () => {
   const { isLoggedIn } = useSelector((state) => state.user);
@@ -43,7 +42,6 @@ const AppRouter = () => {
             <Route path="/" element={<Home />}>
               <Route path="home" element={<Widget />} />
               <Route path="modifyprofile" element={<ModifyProfile />} />
-              <Route path="modifypassword" element={<ModifyPassword />} />
               <Route path="notice" element={<Notice />}>
                 <Route path="" element={<NoticeList />} />
                 <Route path="register" element={<NoticeRegister />} />
