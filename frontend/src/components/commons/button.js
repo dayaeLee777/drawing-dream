e82af7dfb1b8861f5c48714c1e78516159080fe9
runkey <csi9509@naver.com>
2022-02-14 12:56:55 +0900

    style: 버튼에 마우스 갖다 댈 시 커서 모양 변경

--- frontend/src/components/commons/button.js
+++ frontend/src/components/commons/button.js
@@ -14,6 +14,7 @@ const StyledButton = styled.button`
   border-radius: 5px;
   font-family: "Noto Sans KR", sans-serif;
   transition: 0.2s;
+  cursor: pointer;
 
   &:hover {
     background-color: ${(props) =>
