deb9ad0ba4b4efe1f8e2d5e152faaebca7c8ed13
runkey <csi9509@naver.com>
2022-02-14 15:45:57 +0900

    style: 홈 화면 위젯 배경 색상 변경 및 border-radius: 10px; 적용

--- frontend/src/components/widgets/Score.js
+++ frontend/src/components/widgets/Score.js
@@ -4,10 +4,11 @@ import Chart from "./score/Chart";
 
 const Container = styled(motion.div)`
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-  background-color: ${({ theme }) => theme.widgetColor};
+  background-color: ${({ theme }) => theme.ContainerColor};
   display: flex;
   flex-direction: column;
   align-items: center;
+  border-radius: 10px;
 `;
 
 const Wrapper = styled.div`
