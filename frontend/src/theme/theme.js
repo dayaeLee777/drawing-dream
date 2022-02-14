8f738baea82ef185f964bda8d46fb32265d8b183
runkey <csi9509@naver.com>
2022-02-14 12:27:05 +0900

    style: 테마 색상 추가

--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -2,18 +2,22 @@ export const lightTheme = {
   bgColor: "#FFFFFF",
   textColor: "#000000",
   widgetColor: "#FFFFFF",
+  ContainerColor: "#FFFFFF",
   dropdownColor: "#FFFFFF",
   menuColor: "#828282",
   timetableBorder: "1px solid #FFFFFF",
   timetableColor: "#F2F2F2",
   memoBorder: "1px solid #828282",
   memoColor: "#555555",
+  noticeHoverColor: "#f2f2f2",
+  chatFilter: "drop-shadow(6px 6px 5px #828282)",
 };
 
 export const darkTheme = {
   bgColor: "#292929",
   textColor: "#FFFFFF",
   menuColor: "#828282",
+  widgetColor: "#7C7C7C",
   dropdownColor: "#4D4C4C",
   widgetColor: "#605F5F",
   ContainerColor: "#7C7C7C",
@@ -22,6 +26,8 @@ export const darkTheme = {
   timetableColor: "#454545",
   memoBorder: "1px solid #A19F9F",
   memoColor: "#A19F9F",
+  noticeHoverColor: "#646464",
+  chatFilter: "drop-shadow(6px 6px 5px black)",
 };
 
 export const theme = {
