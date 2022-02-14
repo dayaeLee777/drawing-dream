cf0a59431367a6c605fa71d14ef5a39ccd4249aa
runkey <csi9509@naver.com>
2022-02-14 13:08:19 +0900

    style: 각 멤버 그림자 효과 변경

--- frontend/src/components/classroom/Member.js
+++ frontend/src/components/classroom/Member.js
@@ -13,7 +13,7 @@ import { closeChat, openChat } from "modules/chat";
 const Container = styled(motion.div)`
   width: 160px;
   height: 160px;
-  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  box-shadow: rgb(50 50 50) 0px 2px 8px 0px;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
