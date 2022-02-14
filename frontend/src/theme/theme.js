f108a0df48639c1064eb47632e5ee56b0ad87f91
runkey <csi9509@naver.com>
2022-02-14 10:51:39 +0900

    style: Nav-DropDown 배경 색 추가

--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -2,9 +2,10 @@ export const lightTheme = {
   bgColor: "#FFFFFF",
   textColor: "#000000",
   widgetColor: "#FFFFFF",
+  dropdownColor: "#FFFFFF",
   menuColor: "#828282",
-  timetableBorder: "1px solid #ffffff",
-  timetableColor: "#f2f2f2",
+  timetableBorder: "1px solid #FFFFFF",
+  timetableColor: "#F2F2F2",
   memoBorder: "1px solid #828282",
   memoColor: "#555555",
 };
@@ -13,13 +14,14 @@ export const darkTheme = {
   bgColor: "#292929",
   textColor: "#FFFFFF",
   menuColor: "#828282",
+  dropdownColor: "#4D4C4C",
   widgetColor: "#605F5F",
   ContainerColor: "#7C7C7C",
   classColor: "#454545",
   timetableBorder: "1px solid #605F5F",
   timetableColor: "#454545",
-  memoBorder: "1px solid #a19f9f",
-  memoColor: "#a19f9f",
+  memoBorder: "1px solid #A19F9F",
+  memoColor: "#A19F9F",
 };
 
 export const theme = {
