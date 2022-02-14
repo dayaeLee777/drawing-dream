eb769499e92a917e0a3c7acead6ec18637ad46bb
runkey <csi9509@naver.com>
2022-02-14 10:31:03 +0900

    style: 시간표 n교시 배경 및 테두리 색 지정

--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -3,6 +3,7 @@ export const lightTheme = {
   textColor: "#000000",
   widgetColor: "#FFFFFF",
   menuColor: "#828282",
+  timetableBorder: "1px solid #ffffff",
 };
 
 export const darkTheme = {
@@ -12,6 +13,7 @@ export const darkTheme = {
   widgetColor: "#605F5F",
   ContainerColor: "#7C7C7C",
   classColor: "#454545",
+  timetableBorder: "1px solid #605F5F",
 };
 
 export const theme = {
