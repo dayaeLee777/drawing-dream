15585d17d5af30e7920882a3029f2f3cee3bb7e1
runkey <csi9509@naver.com>
2022-02-14 09:25:50 +0900

    style: 모아보기 위젯 테두리 설정

--- frontend/src/components/widgets/Widget.js
+++ frontend/src/components/widgets/Widget.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Container = styled(motion.div)`
   height: 5rem;
-  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  box-shadow: rgb(50 50 50) 0px 2px 8px 0px;
   display: flex;
   justify-content: center;
   align-items: center;
