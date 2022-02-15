b0478cf9831a4ab7735d73857ecaeb89df54ccef
runkey <csi9509@naver.com>
2022-02-15 16:43:30 +0900

    feature: 스터디 화면 구성

--- frontend/src/routes/content/study.js
+++ frontend/src/routes/content/study.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Outlet } from "react-router-dom";
+import StudyList from "components/layout/study/StudyList";
 
 const Container = styled.div`
-  width: 100%;
   box-sizing: border-box;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   border-radius: 5px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: ${({ theme }) => theme.ContainerColor};
   margin-bottom: 5rem;
-  padding-bottom: 3rem;
+  background-color: ${({ theme }) => theme.ContainerColor};
   height: 80vh;
 `;
 
-const School = () => {
+const Title = styled.div`
+  font-size: 1.8rem;
+  margin: 3rem 0;
+  font-weight: 600;
+`;
+
+const InnerContainer = styled.div`
+  width: 100%;
+  margin-bottom: 3rem;
+`;
+
+const Study = () => {
   return (
     <Container>
-      <Outlet />
+      <Title>스터디</Title>
+      <InnerContainer>
+        <StudyList />
+      </InnerContainer>
     </Container>
   );
 };
 
-export default School;
+export default Study;
