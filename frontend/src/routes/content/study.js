469db0db1c0b43889d65395c5325c4f9047ea9ac
runkey <csi9509@naver.com>
2022-02-15 16:18:27 +0900

    feature: 스터디 페이지 만듦

---
+++ frontend/src/routes/content/study.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import { Outlet } from "react-router-dom";
+
+const Container = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${({ theme }) => theme.ContainerColor};
+  margin-bottom: 5rem;
+  padding-bottom: 3rem;
+  height: 80vh;
+`;
+
+const School = () => {
+  return (
+    <Container>
+      <Outlet />
+    </Container>
+  );
+};
+
+export default School;
